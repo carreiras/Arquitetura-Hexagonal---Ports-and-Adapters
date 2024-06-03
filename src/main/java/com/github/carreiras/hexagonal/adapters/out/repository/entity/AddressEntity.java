dc333b63e6c1dff8144cb6087ae32138dcafb4b7
Ewerton Carreira <ewertoncarreira@gmail.com>
2024-06-03 13:31:07 -0300

    Criação do CRUD - CREATE - repository

---
+++ src/main/java/com/github/carreiras/hexagonal/adapters/out/repository/entity/AddressEntity.java
@@ -0,0 +1,14 @@
+package com.github.carreiras.hexagonal.adapters.out.repository.entity;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Data;
+
+@Data
+@Document(collection = "addresses")
+public class AddressEntity {
+
+    private String street;
+    private String city;
+    private String state;    
+}
